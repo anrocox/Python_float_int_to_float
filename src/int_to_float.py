1f72e35e3beed278daa6d5be531b3e3db69e8a1a
Andres Rodriguez <anrocox@gmail.com>
2014-07-17 14:27:04 -0500

    In Python how to convert a integer to float?

---
+++ src/int_to_float.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on Jul 16, 2014
+
+@author: anroco
+
+In Python how to convert a integer to float?
+
+En Python, ¿Cómo convertir un integer a float?
+'''
+
+#create a integer number
+i = 359
+print(type(i))
+print(i)
+
+#convert using the float() function built-in python
+f = float(i)
+print(type(f))
+print(f)
+
+#create a negative integer number
+i = -51
+print(i)
+
+#convert a negative integer to float
+f = float(i)
+print(f)
